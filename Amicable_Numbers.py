d83ab43fe5b8a3df38940e2c081d7ea38fb64ce9
Navya bindu dwarampudi <123063423+22p31a4211@users.noreply.github.com>
2023-05-25 21:21:37 +0530

    def fact(n):     s=0     for i in range(1,n):         if(n%i==0):             s+=i     return s n=int(input()) m=int(input()) s=fact(n) x=fact(m) if(n==x and s==m):     print('Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,19 +1,14 @@
+def fact(n):
+    s=0
+    for i in range(1,n):
+        if(n%i==0):
+            s+=i
+    return s
 n=int(input())
 m=int(input())
-ns=0
-ms=0
-for i in range(1,n+1):
-    if n==i:
-        break
-    if n%i==0:
-       ns=ns+i
-for i in range (1,m+1):
-    if m==i:
-        break
-    if m%i==0:
-        ms+=i
-if ms==n and ns==m:
-    print("Amicable")
+s=fact(n)
+x=fact(m)
+if(n==x and s==m):
+    print('Amicable')
 else:
-    print("Not Amicable")
-    
+    print('Not Amicable')
