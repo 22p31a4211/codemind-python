cc4f905e5c5bccdf1ac7eac23888b6811eedb682
Navya bindu dwarampudi <123063423+22p31a4211@users.noreply.github.com>
2023-05-25 21:16:02 +0530

    n=int(input()) s=str(n**2) c=0 t=n k='' while(n):     c+=1     n//=10 for i in range(len(s)-1,len(s)-c-1,-1):     k+=s[i] f='' for i in range(len(k)-1,-1,-1):     f+=k[i] if((f)==str(t)):     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,16 +1,17 @@
 n=int(input())
-a=10
-sq=0
-i=0
-while True:
-    if n<a:
-        sq=n**2
-        d=sq%a
-        if n==d:
-            print("Automorphic Number")
-            break
-        else:
-            print("Not an Automorphic Number")
-            break
-    a=a*10
-    i+=1
+s=str(n**2)
+c=0
+t=n
+k=''
+while(n):
+    c+=1
+    n//=10
+for i in range(len(s)-1,len(s)-c-1,-1):
+    k+=s[i]
+f=''
+for i in range(len(k)-1,-1,-1):
+    f+=k[i]
+if((f)==str(t)):
+    print('Automorphic Number')
+else:
+    print('Not an Automorphic Number')
